ea886ff943574346c384c09e3857a9537e84aea9
William Poindexter <wpoindexter@bimfizzle.com>
2014-09-15 22:15:44 -0400

    sign in dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def sale_price(original_price, discount_percentage)
     number_to_currency original_price.to_f * (1 - (discount_percentage.to_f * 0.01))
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
